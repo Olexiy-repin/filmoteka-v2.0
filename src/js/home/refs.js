cfcd295b8b03190087738d00b47afaf244b69dce
Olexiy-repin <repinlyoha@gmail.com>
2022-02-03 21:46:18 +0200

    nearly final

---
+++ src/js/home/refs.js
@@ -0,0 +1,12 @@
+export const refs = {
+  //Gallery Elements
+  filmGallery: document.querySelector('.js-gallery-list'),
+  //Modal Elements
+  backdrop: document.querySelector('.js-backdrop'),
+  modalCardContainer: document.querySelector('.js-modal__card-container'),
+  //Form Elements
+  searchForm: document.querySelector('.js-search-form'),
+  warningText: document.querySelector('.js-warning-message'),
+  //Pagination Elements
+  paginationContainer: document.querySelector('.js-tui-pagination'),
+};
---
+++ src/js/library/refs.js
@@ -0,0 +1,11 @@
+export const refs = {
+  //Gallery Elements
+  filmGallery: document.querySelector('.js-gallery-list'),
+  //Modal Elements
+  backdrop: document.querySelector('.js-backdrop'),
+  modalCardContainer: document.querySelector('.js-modal__card-container'),
+  //Pagination Elements
+  paginationContainer: document.querySelector('.js-tui-pagination'),
+  //Filter Buttons
+  filterBtnsContainer: document.querySelector('.js-utils-list'),
+};
